9e42eefb427aea8537475aba03c2c79b27f8a799
Bruno Ramos <b.ramos@hotmail.com>
2022-05-05 22:44:07 -0300

    v1 appl

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MovimentacaoListComponent } from "./components/movimentacao-list/movimentacao-list.component"
+import { MovimentacaoNewComponent } from "./components/movimentacao-new/movimentacao-new.component"
+import { CorrentistaComponent } from './components/correntista/correntista.component';
+
+const routes: Routes = [
+  { path: 'movimentacoes-new', component: MovimentacaoNewComponent},
+  { path: 'movimentacoes', component: MovimentacaoListComponent},
+  { path: '', redirectTo: 'movimentacoes', pathMatch: 'full'},
+  { path: 'correntistas', component: CorrentistaComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
